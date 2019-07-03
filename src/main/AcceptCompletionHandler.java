6192f3601ea6a0ba75ae0ea369f5dbdbabaa8db4
sansey <sansey@sansey-pc>
2019-07-03 09:45:34 +0300

    Started simple implementation of handlers

--- src/main/AcceptCompletionHandler.java
+++ src/main/AcceptCompletionHandler.java
@@ -1,13 +1,23 @@
 package main;
 
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class AcceptCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
+	//TODO need separate buffers for every read-handler instance
+
 	@Override
-	public void completed(AsynchronousSocketChannel result,	AsynchronousServerSocketChannel server) {
+	public void completed(AsynchronousSocketChannel client,	AsynchronousServerSocketChannel server) {
 		System.out.println("Server: " + Thread.currentThread().getName()+ " accepted connection.");
+		server.accept(server, this);
+		// I like this call, but should test timeouts
+		// read until "exit" sustring is reached in input. Then return the input to client
+		client.read(buffer, 0, TimeUnit.SECONDS, attachment, handler);
 	}
 
 	@Override
@@ -44,4 +54,40 @@ public void completed(AsynchronousSocketChannel result, Object attachment) {
      // process error
  }
 
-});			
+});	
+
+
+
+
+class ReadWriteHandler implements
+CompletionHandler<Integer, Map<String, Object>> {
+   
+  @Override
+  public void completed(
+    Integer result, Map<String, Object> attachment) {
+      Map<String, Object> actionInfo = attachment;
+      String action = (String) actionInfo.get("action");
+
+      if ("read".equals(action)) {
+          ByteBuffer buffer = (ByteBuffer) actionInfo.get("buffer");
+          buffer.flip();
+          actionInfo.put("action", "write");
+
+          clientChannel.write(buffer, actionInfo, this);
+          buffer.clear();
+
+      } else if ("write".equals(action)) {
+          ByteBuffer buffer = ByteBuffer.allocate(32);
+
+          actionInfo.put("action", "read");
+          actionInfo.put("buffer", buffer);
+
+          clientChannel.read(buffer, actionInfo, this);
+      }
+  }
+   
+  @Override
+  public void failed(Throwable exc, Map<String, Object> attachment) {
+      // 
+  }
+}
