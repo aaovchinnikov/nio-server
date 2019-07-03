3f270791dc32f9f5a5c3c694add0dc0c8112a230
sansey <sansey@sansey-pc>
2019-07-03 09:33:18 +0300

    Semaphore approach is wrong because AcceptPendingException thrown
    This approved for Linux at least.
    Move to one acceptor thread.

--- src/main/AcceptCompletionHandler.java
+++ src/main/AcceptCompletionHandler.java
@@ -1,17 +1,47 @@
 package main;
 
+import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
-import java.util.concurrent.Semaphore;
 
-public class AcceptCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, Semaphore> {
+public class AcceptCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
 	@Override
-	public void completed(AsynchronousSocketChannel result,	Semaphore semaphore) {
-		semaphore.release();
+	public void completed(AsynchronousSocketChannel result,	AsynchronousServerSocketChannel server) {
+		System.out.println("Server: " + Thread.currentThread().getName()+ " accepted connection.");
 	}
 
 	@Override
-	public void failed(Throwable exc, Semaphore semaphore) {
-		semaphore.release();
+	public void failed(Throwable exc, AsynchronousServerSocketChannel server) {
+		System.out.println("Server: " + Thread.currentThread().getName()+ " failed accepting connection.");
+		exc.printStackTrace();
 	}
 }
+
+
+
+@Override
+public void completed(AsynchronousSocketChannel result, Object attachment) {
+    // we have access to field "server" and create another accept-request to OS in thread pool.
+	// all the time we have one accepting thread in thread pool
+	if (server.isOpen()){
+        server.accept(null, this);
+    }
+
+    clientChannel = result;
+    if ((clientChannel != null) && (clientChannel.isOpen())) {
+        ReadWriteHandler handler = new ReadWriteHandler();
+        ByteBuffer buffer = ByteBuffer.allocate(32);
+
+        Map<String, Object> readInfo = new HashMap<>();
+        readInfo.put("action", "read");
+        readInfo.put("buffer", buffer);
+
+        clientChannel.read(buffer, readInfo, handler);
+     }
+ }
+ @Override
+ public void failed(Throwable exc, Object attachment) {
+     // process error
+ }
+
+});			
--- src/main/Server.java
+++ src/main/Server.java
@@ -4,19 +4,14 @@ import java.io.IOException;
 import java.net.SocketAddress;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
-import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.channels.CompletionHandler;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 
 public class Server {
 	private final SocketAddress socket;
-	private final Semaphore semaphore;
 
 	
-	public Server(SocketAddress socket, Semaphore semaphore) {
+	public Server(SocketAddress socket) {
 		this.socket = socket;
-		this.semaphore = semaphore;
 	}
 	
 	public void start() throws IOException, InterruptedException {
@@ -25,39 +20,7 @@ public class Server {
 			AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group);
 			server.bind(socket);
 			while(true) {
-				// CompletionHandler is inner anonymous class? It seems to be the reason why it has access to outer class fields.
-			    server.accept(null, new CompletionHandler<AsynchronousSocketChannel,Object>() {
-			    	@Override
-	    	        public void completed(AsynchronousSocketChannel result, Object attachment) {
-	    	            // we have access to field "server" and create another accept-request to OS in thread pool.
-			    		// all the time we have one accepting thread in thread pool
-			    		if (server.isOpen()){
-	    	                server.accept(null, this);
-	    	            }
-	    	 
-	    	            clientChannel = result;
-	    	            if ((clientChannel != null) && (clientChannel.isOpen())) {
-	    	                ReadWriteHandler handler = new ReadWriteHandler();
-	    	                ByteBuffer buffer = ByteBuffer.allocate(32);
-	    	 
-	    	                Map<String, Object> readInfo = new HashMap<>();
-	    	                readInfo.put("action", "read");
-	    	                readInfo.put("buffer", buffer);
-	    	 
-	    	                clientChannel.read(buffer, readInfo, handler);
-	    	             }
-	    	         }
-	    	         @Override
-	    	         public void failed(Throwable exc, Object attachment) {
-	    	             // process error
-	    	         }
-	 
-	    	    });			
-				
-				
-				
-				this.semaphore.acquire();
-				server.accept(this.semaphore, new AcceptCompletionHandler());
+				server.accept(server, new AcceptCompletionHandler());
 				if(Thread.currentThread().isInterrupted()) {
 					group.shutdownNow();
 					break;
